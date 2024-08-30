579fb4c9804b7a4c417a9484975de7e95a7d5013
Sawera Jamshaid <jsawera510@gmail.com>
2024-08-30 14:12:03 +0500

    condition check

---
+++ main.py
@@ -0,0 +1,9 @@
+weight = float(input("enter your weight: "))
+ 
+if weight <= 18.5:
+    print("under weight")
+elif weight >= 18.5 and weight > 25.0:
+    print("normal weight")
+else:
+    print("over weight")      
+
