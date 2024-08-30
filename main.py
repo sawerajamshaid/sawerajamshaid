49a72961fdf481e138e4189ebc887a1fc3112f16
Sawera  Jamshaid <jsawera510@gmail.com>
2024-08-30 14:13:49 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# weight = float(input("enter your weight: "))
+ 
+# if weight <= 18.5:
+#     print("under weight")
+# elif weight >= 18.5 and weight > 25.0:
+#     print("normal weight")
+# else:
+# #     print("over weight")      
+
+
+# a = int(input("enter your number:"))
+# if a% 2 == 0:
+#     print("even")
+# else:
+#     ("odd")
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Great A")
+elif marks >= 80:
+    print("Great B")
+elif marks >= 70:
+    print("Great C")
+elif marks >= 50:
+    print("Great E")
+else:
+    print("Fail")
+
+print("Happy")  
+
